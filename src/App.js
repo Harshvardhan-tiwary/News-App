3a79c18b463879ceec8a73686ccbd956d09b1b39
Harshvardhan-tiwary <tiwaryharsh12169@gmail.com>
2022-12-25 18:29:21 +0530

    Scrolling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import NewsCards from './componenets/NewsCards/NewsCards';
+import useStyles from './styles.js';
 
 const alankey = 'df562e911aa8dbb0be7a2623da090afa2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
     const [newsArticles, setNewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
+    const classes = useStyles();
 
     useEffect(() => {
         alanBtn({
@@ -14,6 +17,9 @@ const App = () => {
             onCommand: ({ command, articles}) => {
                 if(command === 'newHeadlines'){
                     setNewsArticles(articles);
+                    setActiveArticle(-1);
+                } else if(command === 'highlight'){
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
                 }
 
             }
@@ -22,8 +28,11 @@ const App = () => {
 
     return (
     <div>
-        <h1>AI News</h1>
-        <NewsCards articles={newsArticles} />
+        <div className={classes.logoContainer}>
+            
+        <img src="https://t4.ftcdn.net/jpg/01/66/52/39/240_F_166523930_XzLaIVUlXha2AB6JgRShWp2je0FBLnJi.jpg" className={classes.alanLogo} alt="News logo" />
+        </div>
+        <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
     </div>
     );
 }
